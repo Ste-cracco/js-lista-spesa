612b6b6abf5bb8063bdaa8814a9fa8fac20fa343
Ste-cracco <s.cracco@yahoo.it>
2022-07-27 14:12:47 +0200

    Stampa lista della spesa con ciclo "while"

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,15 +3,33 @@ const listaSpesa = ["pasta", "insalata", "mele", "limoni", "banane"]
 const ul = document.querySelector('.lista_spesa');
 
 
-for(let i = 0; i < listaSpesa.length; i++) {
+// ciclo for
+
+// for(let i = 0; i < listaSpesa.length; i++) {
     
-    // creazione <li> e aggiunta classe .elemento_spesa
+//     // creazione <li> e aggiunta classe .elemento_spesa
+//     const liElement = document.createElement('li');
+//     liElement.classList.add('elemento_spesa');
+
+//     // aggiungo a <li> gli oggetti dell'array listaSpesa
+//     liElement.append(listaSpesa[i]);
+
+//     // Aggiunto <li> a <ul> con classe .lista_spesa
+//     ul.append(liElement);
+// }
+
+// ciclo while
+
+let i = 0;
+
+while (i < listaSpesa.length) {
+
     const liElement = document.createElement('li');
-    liElement.classList.add('elemento_spesa')
+    liElement.classList.add('elemento_spesa');
 
-    // aggiungo a <li> gli oggetti dell'array listaSpesa
-    liElement.append(listaSpesa[i])
+    liElement.append(listaSpesa[i]);
 
-    // Aggiunto <li> a <ul> con classe .lista_spesa
     ul.append(liElement);
+
+    i++;
 }
